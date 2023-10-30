c9fb639640e4a3d4f79df2e68ff31e54caaa6e52
radelytskyipavlo <145326273+radelytskyipavlo@users.noreply.github.com>
2023-10-30 20:03:32 +0200

    Add files via upload

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab5.2/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double x = 0.5;
+			double eps = 0.0000001;
+			int n = 0;
+			double s = 0;
+			double expected = atan(x);
+			double actual = S(x, eps, n, s);
+			Assert::AreEqual(expected, actual, eps);
+		}
+	};
+}
